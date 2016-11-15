01bc7e52fe59ddd7a34aa6d321e412e93b9bd733
Kevin Matthews <kmatthews@cyberdata.net>
2016-11-14 18:12:15 -0800

    Add a redirect using redirect() and url_for().

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 # Browse to this URL to view the page defined by the hello_world() function.
@@ -8,13 +8,20 @@ app = Flask(__name__)
 def index():
     return 'Welcome to my webpage! Browse to /hello, /goodbye, /hello_NAME, or /goodbye_NAME'
 
+@app.route('/admin')
+def hello_admin():
+    return 'Welcome to Admin Land!'
+
 @app.route('/hello')
 def hello_world():
     return 'Hello, world of Flask!'
 
 @app.route('/hello_<user>')
 def hello_user(user):
-    return 'Hello, %s!' % (user)
+    if user == 'admin':
+        return redirect(url_for('hello_admin')) # Pass the function name, not the URL (hence url_FOR).
+    else:
+        return 'Hello, %s!' % (user)
 
 # This is equivalent to the route decorator.
 def goodbye_world():
